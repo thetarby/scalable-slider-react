2aea4fb8e9ae3c632ea98b804d052d5dcbe7e22e
Furkan Akyol <furkanakyol1997@gmail.com>
2020-04-14 13:29:38 +0300

    Updates

---
+++ precache-manifest.482db2e35409524343ce4fc5488f3c3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b2e2c0106d2ca507fd66b57b3483c97",
+    "url": "/scalable-slider-react/index.html"
+  },
+  {
+    "revision": "eae07126ff59f653cd0e",
+    "url": "/scalable-slider-react/static/css/main.578f5343.chunk.css"
+  },
+  {
+    "revision": "4b4afd7af11bb6c92a36",
+    "url": "/scalable-slider-react/static/js/2.7274df91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scalable-slider-react/static/js/2.7274df91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eae07126ff59f653cd0e",
+    "url": "/scalable-slider-react/static/js/main.8a1cd4ae.chunk.js"
+  },
+  {
+    "revision": "c05657269f7ae32b8f0a",
+    "url": "/scalable-slider-react/static/js/runtime-main.592ff31c.js"
+  }
+]);
